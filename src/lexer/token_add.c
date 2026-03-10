e222b8bf3ad9f6689ba5bc53ad97d1054ec28d94
Ramis Cesar Damasceno <rdamasce@c1r4p3.42sp.org.br>
2026-03-10 20:04:08 -0300

    finish function add token back

---
+++ src/lexer/token_add.c
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   token_add.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rdamasce <rdamasce@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/09 20:07:23 by rdamasce          #+#    #+#             */
+/*   Updated: 2026/03/10 20:03:28 by rdamasce         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../headers/lexer.h"
+#include "../../headers/structs.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+void add_token_back(t_token **head, t_token *new)
+{
+	t_token *current = *head;
+
+	if (*head == NULL)
+	{
+		*head = new;
+		return;
+	}
+	while (current->next != NULL)
+	{
+		current= current->next;
+	}
+	if (current->next == NULL)
+	{
+		current->next = new;
+		new->prev = current;
+	}
+}
+// Testando funcao.
+/* void main()
+{
+	t_token *head = NULL;
+
+	t_token *token1 = malloc(sizeof(t_token));
+	token1->cmd = "ls";
+	token1->type = 1;
+	token1->next = NULL;
+	token1->prev = NULL;
+
+	add_token_back(&head, token1);
+
+	printf("Teste 1 - Lista com 1 elemento: \n");
+	printf("Head cmd: %s\n", head->cmd); // deve imprimir "ls"
+	printf("Head prev: %p\n", head->prev); // deve imprimir NULL
+	printf("Head next: %p\n", head->next); // deve imprimir NULL
+
+	t_token *token2 = malloc(sizeof(t_token));
+	token2->cmd = "pwd";
+	token2->type = 2;
+	token2->next = NULL;
+	token2->prev = NULL;
+
+	add_token_back(&head, token2);
+
+	printf("Teste 2 - Lista com 2 elementos:\n");
+	printf("Head cmd: %s\n", head->cmd);
+	printf("Segundo comando cmd: %s\n", head->next->cmd);
+	printf("Segundo comando prev cmd: %s\n", head->next->prev->cmd);
+	printf("Segundo next: %p\n", head->next->next);
+} */
