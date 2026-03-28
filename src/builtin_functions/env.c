eec2b03773e2fe5a70064cf99e08507e3c501276
Vagner Melo <vagnerbrmelo@gmail.com>
2026-03-28 12:17:27 -0300

    implemented env builtin function

--- src/builtin_functions/env.c
+++ src/builtin_functions/env.c
@@ -1,28 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   builtin_functions.h                                :+:      :+:    :+:   */
+/*   env.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vade-mel <vade-mel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/03/02 23:14:12 by vade-mel          #+#    #+#             */
-/*   Updated: 2026/03/05 22:11:08 by vade-mel         ###   ########.fr       */
+/*   Created: 2026/03/28 12:05:00 by vade-mel          #+#    #+#             */
+/*   Updated: 2026/03/28 12:05:00 by vade-mel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "minishell.h"
 
-#ifndef BUILTIN_FUNCTIONS_H
-# define  BUILTIN_FUNCTIONS_H
+void	ft_env(t_mini *ms, char **cmd)
+{
+	int	i;
 
-/**
- * Libs inluded to build the Built-in functions
-*/
-# include <unistd.h>
-
-typedef struct s_mini	t_mini;
-
-void	ft_echo(t_mini *ms, char **cmd);
-void	ft_env(t_mini *ms, char **cmd);
-void	ft_pwd(t_mini *ms, char **cmd);
-
-#endif
+	(void)cmd;
+	if (!ms || !ms->envp)
+	{
+		if (ms)
+			ms->error = 0;
+		return ;
+	}
+	i = 0;
+	while (ms->envp[i])
+	{
+		ft_printf("%s\n", ms->envp[i]);
+		i++;
+	}
+	ms->error = 0;
+}
